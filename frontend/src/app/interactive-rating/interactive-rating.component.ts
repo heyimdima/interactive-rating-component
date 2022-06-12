aa51a86130f5c810439e3fff9352f8213e49f789
heyimdima <bondardima@icloud.com>
2022-06-11 23:49:23 -0800

    what am i doing

--- frontend/src/app/interactive-rating/interactive-rating.component.ts
+++ frontend/src/app/interactive-rating/interactive-rating.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-interactive-rating',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InteractiveRatingComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
